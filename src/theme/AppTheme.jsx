5a13855bc20dfec59a721bd5172da4e3b77e8ae1
Jesus Gastelum <gastelumtec21@gmail.com>
2023-04-04 20:46:07 -0700

    AuthLayout, LoginPage y RegisterPage creadas

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,9 +1,9 @@
-import { ThemeProvider } from "@emotion/react"
-import { CssBaseline } from "@mui/material"
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { purpleTheme } from './'
 
 export const AppTheme = ({ children }) => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={purpleTheme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
       { children } 
